b778f228f8c843f00fa4de5384f2a6b250df0eb1
Dasiane Jones <dasianejones@gmail.com>
2020-02-06 12:21:28 -0500

    updated app.js file in src folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,38 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home.js";
+import Restaurants from "./components/Restaurants";
+import SingleRestaurant from "./components/SingleRestaurant";
+import Reviews from "./components/Reviews";
+import SingleReview from "./components/SingleReview";
+import Comments from "./components/Comments";
+import SingleComment from "./components/SingleComment";
+import Login from "./components/Login";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/restaurants" component={Restaurants} />
+          <Route
+            exact
+            path="/restaurants/restaurants:sId"
+            component={SingleRestaurant}
+          />
+          <Route exact path="/reviews" component={Reviews} />
+          <Route exact path="/reviews/:reviewsId" component={SingleReview} />
+          <Route exact path="/comments" component={Comments} />
+          <Route exact path="/comments/:commentsId" component={SingleComment} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hey Boo, We Out Here Live Or Wuheva!</h1>
+      </div>
+    );
+  }
+}
