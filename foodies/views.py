99508521dd487132241cd8e40b4072debe6ac9f1
Dasiane Jones <dasianejones@gmail.com>
2020-02-06 11:09:25 -0500

    updated views.py

--- foodies/views.py
+++ foodies/views.py
@@ -1,3 +1,20 @@
+from rest_framework import viewsets
 from django.shortcuts import render
 
-# Create your views here.
+from .serializers import RestaurantSerializer, ReviewSerializer, CommentSerializer
+from .models import Restaurant, Review, Comment
+
+
+class RestaurantView(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+
+class ReviewView(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class CommentView(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
