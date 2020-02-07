69c3bbb876ae80374c643ce18b6787bc826b1d9c
Dasiane Jones <dasianejones@gmail.com>
2020-02-07 10:51:54 -0500

    updated singlereview.js with functions

--- client/src/components/SingleReview.js
+++ client/src/components/SingleReview.js
@@ -14,6 +14,45 @@ export default class SingleReview extends Component {
     },
     redirect: false
   };
+
+  updateSingleReview = () => {
+    const url = `/api/v1/review/${this.props.match.params.reviewsId}`;
+    axios.get(url).then(res => {
+      this.setState({ redirect: true });
+    });
+  };
+
+  componentDidMount() {
+    console.log("reviewsId", this.props.match.params.reviewsId);
+    axios
+      .get(`/api/v1/review/${this.props.match.params.reviewsId}`)
+      .then(res => {
+        console.log("res.data", res.data);
+        this.setState({ review: res.data });
+      });
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    const url = `/api/v1/review/${this.props.match.params.reviewsId}`;
+    axios.put(url, this.state.review);
+  };
+
+  deleteSingleComment = () => {
+    const url = `/api/v1/review/${this.props.match.params.reviewsId}`;
+    axios.delete(url).then(() => {
+      this.setState({ redirect: true });
+    });
+  };
+
+  onChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    const newState = { ...this.state };
+    newState.review[name] = value;
+    this.setState(newState);
+  };
   render() {
     return <div>Hi SingleReview! Im New Here!</div>;
   }
