b033d5ef4c51a3bc1b2df344965db0f45a423aea
Dasiane Jones <dasianejones@gmail.com>
2020-02-07 10:49:37 -0500

    updated singlrestaurant.js with functions

--- client/src/components/SingleRestaurant.js
+++ client/src/components/SingleRestaurant.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import axios from "axios";
+import { Link } from "react-router-dom";
 export default class SingleRestaurant extends Component {
   state = {
     restaurant: [],
@@ -13,6 +14,45 @@ export default class SingleRestaurant extends Component {
       need_directions: ""
     },
     redirect: false
+  };
+
+  updateSingleRestaurant = () => {
+    const url = `/api/v1/restaurant/${this.props.match.params.restaurantsId}`;
+    axios.get(url).then(res => {
+      this.setState({ redirect: true });
+    });
+  };
+
+  componentDidMount() {
+    console.log("restaurantsId", this.props.match.params.restaurantsId);
+    axios
+      .get(`/api/v1/restaurant/${this.props.match.params.restaurantsId}`)
+      .then(res => {
+        console.log("res.data", res.data);
+        this.setState({ restaurant: res.data });
+      });
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    const url = `/api/v1/restaurant/${this.props.match.params.restaurantsId}`;
+    axios.put(url, this.state.restaurant);
+  };
+
+  deleteSingleComment = () => {
+    const url = `/api/v1/restaurant/${this.props.match.params.restaurantsId}`;
+    axios.delete(url).then(() => {
+      this.setState({ redirect: true });
+    });
+  };
+
+  onChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    const newState = { ...this.state };
+    newState.restaurant[name] = value;
+    this.setState(newState);
   };
   render() {
     return <div>Hi SingleRestaurant! Im New Here!</div>;
