85c6b26778807e8e325214de7f9dadb252720c77
Dasiane Jones <dasianejones@gmail.com>
2020-02-07 10:39:56 -0500

    updated reviews.js file with functions

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -14,6 +14,46 @@ export default class Reviews extends Component {
     },
     redirect: false
   };
+
+  componentDidMount() {
+    this.updateReviewsList();
+  }
+
+  updateReviewsList = () => {
+    axios.get("/api/v1/review/").then(res => {
+      this.setState({ reviews: res.data });
+      console.log(this.state.reviews);
+    });
+  };
+
+  onChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    const newState = { ...this.state };
+    newState.newReviews[name] = value;
+    this.setState(newState);
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    axios.post("/api/v1/review/", this.state.newReviews).then(() => {
+      const newState = { ...this.state };
+      newState.newReviews = {
+        reviewer_name: "",
+        restaurant_name: "",
+        dish_rating: "",
+        favorite_dish: "",
+        customer_service_rating: "",
+        dish_picture: "",
+        restaurant: ""
+      };
+      this.setState(newState);
+      // update page data
+      this.updateReviewsList();
+    });
+  };
   render() {
     return <div>Hi Reviews! Im New Here!</div>;
   }
