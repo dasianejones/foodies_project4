6d4bcaf852c1afaa4e1183d36a40186148488335
Dasiane Jones <dasianejones@gmail.com>
2020-02-07 10:28:25 -0500

    updated state in comments.js file

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 
 export default class Comments extends Component {
+  state = {
+    comments: [],
+    newComments: {
+      commenter_name: "",
+      commenter_picture: "",
+      comment_heading: "",
+      commenter_location: "",
+      content: "",
+      review: ""
+    },
+    redirect: false
+  };
   render() {
     return <div>Hi Comments! Im New Here!</div>;
   }
