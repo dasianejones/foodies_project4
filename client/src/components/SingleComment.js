8b839f1e5cf65cb106d498cc84fd372d18da11ae
Dasiane Jones <dasianejones@gmail.com>
2020-02-07 10:46:06 -0500

    updated singlecomment.js with functions

--- client/src/components/SingleComment.js
+++ client/src/components/SingleComment.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class SingleComment extends Component {
   state = {
@@ -13,6 +15,46 @@ export default class SingleComment extends Component {
     },
     redirect: false
   };
+
+  updateSingleComment = () => {
+    const url = `/api/v1/comment/${this.props.match.params.commentsId}`;
+    axios.get(url).then(res => {
+      this.setState({ redirect: true });
+    });
+  };
+
+  componentDidMount() {
+    console.log("commentsId", this.props.match.params.commentsId);
+    axios
+      .get(`/api/v1/comment/${this.props.match.params.commentsId}`)
+      .then(res => {
+        console.log("res.data", res.data);
+        this.setState({ comment: res.data });
+      });
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    const url = `/api/v1/comment/${this.props.match.params.commentsId}`;
+    axios.put(url, this.state.comment);
+  };
+
+  deleteSingleComment = () => {
+    const url = `/api/v1/comment/${this.props.match.params.commentsId}`;
+    axios.delete(url).then(() => {
+      this.setState({ redirect: true });
+    });
+  };
+
+  onChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    const newState = { ...this.state };
+    newState.comment[name] = value;
+    this.setState(newState);
+  };
+
   render() {
     return <div>Hi SingleComment! Im New Here!</div>;
   }
